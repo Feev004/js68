e8ed5589f07fa5905d4448c6cfffd6043f83a47e
Ratthasart Rojjanai <feevfeev2@gmail.com>
2025-12-05 19:08:38 +0700

    up

--- test1.js
+++ test1.js
@@ -1,4 +1,31 @@
-let n = null === undefined
-console.log(n);
-n = null == undefined
-console.log(n);
+num = 0
+if(num > 0 ){
+    console.log("Positive number");
+}
+// else if(num === 0){
+//     console.log("Zero");
+// }
+else{
+    console.log("Non-positive number");
+}
+
+switch(num){
+    case 1:
+        break;
+    case 0:
+        console.log("Zero");
+        break;
+    default:
+        console.log("Non-positive number");
+}
+
+let num1 = 1;
+switch(num1){
+    case '1':
+        console.log("You entered one");
+    case '2':
+        console.log("You entered two");
+    default:
+        console.log("You entered a number other than one or two");
+}
+
