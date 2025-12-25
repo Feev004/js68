2682dfb644e00c89f38e271149a72a72fed72cbf
Ratthasart Rojjanai <feevfeev2@gmail.com>
2025-12-25 09:05:03 +0700

    up

---
+++ week4/learn/7_1_1_Literal_Array.js
@@ -0,0 +1,13 @@
+let test1 = ["สมศรี", "สมชาย", "สมเกียรติ", "สมหญิง"];
+
+console.log(`frist student number (Index 0 ) : ${test1[0]}`);
+console.log(`frist student number (Index 3 ) : ${test1[3]}`);
+console.log(`total student (length) : ${test1.length}`);
+console.log();
+
+console.log(`Before student Index 2 : ${test1[2]}`);
+test1[2] = "ประยุทธ์";
+console.log(`After student Index 2 : ${test1[2]}`);
+
+
+
---
+++ week4/learn/7_1_2_for_Loop.js
@@ -0,0 +1,5 @@
+let subjects = ["Math", "Science", "Thai", "English"];
+console.log("--- รายวิชาทั้งหมด ---");
+for(let i = 0 ; i < subjects.length ; i++){
+    console.log(`subjects ${i+1} : ${subjects[i]}`);
+}
