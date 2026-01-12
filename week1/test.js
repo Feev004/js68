e0479ab3d7a82fde80a067cac9a289d1c908c282
Ratthasart Rojjanai <feevfeev2@gmail.com>
2026-01-12 22:42:15 +0700

    up

--- week1/test.js
+++ week1/test.js
@@ -1,39 +1,55 @@
-let result11 = 10 + 5 * 2
-let result12 = (10+5) * 2
-let result13 = 15%4+1
-let result14 = 4**2/2
+let result11 = 10 + 5 * 2 // 10 + 10 => 20
+let result12 = (10+5) * 2 // 15 * 2 => 30
+let result13 = 15%4+1 // 3 + 1 => 4
+let result14 = 4**2/2 // 16  / 2 => 8
 
-console.log(`num 1.1 : ${result11}`);
-console.log(`num 1.2 : ${result12}`);
-console.log(`num 1.3 : ${result13}`);
-console.log(`num 1.4 : ${result14}`);
-console.log(`\n`);
+// 1.* / % **
+// 2. + - 
+
+// console.log(`num 1.1 : ${result11}`);
+// console.log(`num 1.2 : ${result12}`);
+// console.log(`num 1.3 : ${result13}`);
+// console.log(`num 1.4 : ${result14}`);
+// console.log(`\n`);
 
 
-let a21 = 10
-a21 += 5
-console.log(`num 2.1 : ${a21}`);
-let b22 = 8
-b22 /= 4
-console.log(`num 2.2 : ${b22}`);
-let count = 5
-let a23 = count++
-let b23 = count
-console.log(`num 2.3 : ${a23}, ${b23}`);
-let a24 = ++count
-let b24 = count
-console.log(`num 2.4 : ${a24}, ${b24}`);
-console.log(`\n`);
+// let a21 = 10
+// // a21 += 5 // 10 + 5 + 5 + 5 + 5 + 5 = 35
+// for (let i = 0; i < 5; i++) {
+//   a21 += 5
+// }
+// console.log(`num 2.1 : ${a21}`); // 15
+// let b22 = 8
+// b22 /= 4
+// console.log(`num 2.2 : ${b22}`);
+// let count = 5
+// let c = 5
+// let a23 = count++ // 5
+// let b23 = count // 6
+// console.log(`num 2.3 : ${a23}, ${b23}`);
+// let a24 = ++c // 6
+// let b24 = c // 6
+// console.log(`num 2.4 : ${a24}, ${b24}`);
+// let a25 = c-- // 6
+// let b25 = c // 5
+// console.log(`num 2.4 : ${a25}, ${b25}`);
+// console.log(`\n`);
 
-console.log("num 3.1 : ", 10 === "10");
-console.log("num 3.2 : ", 20 != "20");
-console.log("num 3.3 : ", null >= 0);
-console.log("num 3.4 : ", false == 0);
-console.log(`\n`);
+// let num31 = 10
+// console.log("num 3.1 : ", 10 === "10"); // false '2' == 2 => true / '2' === 2 => false
+// console.log("num 3.2 : ", 20 != "20"); // false
+// console.log("num 3.2 : ", 20 !== "20"); // int !== str => true
+// console.log("num 3.3 : ", null >= 0); // true  null = 0
+// // null undefined false = 0 : true = 1
+// console.log("num 3.4 : ", false === 0); 
+// console.log(`\n`);
 
-let result41 = "20" + 5
+let result41 = "20" + 5 // "20"5 = str : 5 + "20" = int
+let j = 5 + '20'
+console.log(j, typeof j);
+
 console.log(`num 4.1 : ${result41}\t\t`, typeof result41);
-let result42 = "20" - 5
+let result42 = "20" - 5 // 20 - 5
 console.log(`num 4.2 : ${result42}\t\t`, typeof result42);
 let result43 = 1 + true
 console.log(`num 4.3 : ${result43}\t\t\t`,  typeof result43);
--- week1/test1.js
+++ week1/test1.js
@@ -1,16 +1,20 @@
-num = 0
-if(num > 0 ){
-    console.log("Positive number");
-}
+let num = 0;
+// if(num > 0 ){
+//     console.log("Positive number");
+// }
 // else if(num === 0){
 //     console.log("Zero");
+// }else if (num < 0){
+//     console.log("Negative number");
 // }
-else{
-    console.log("Non-positive number");
-}
+// else{
+//     console.log("Non-positive number");
+// }
 
-switch(num){
+let testNum = 1;
+switch(testNum){ // 1
     case 1:
+        console.log('test');
         break;
     case 0:
         console.log("Zero");
@@ -20,11 +24,14 @@ switch(num){
 }
 
 let num1 = 1;
+num1 = '1'
 switch(num1){
     case '1':
         console.log("You entered one");
+        break;
     case '2':
         console.log("You entered two");
+        break;
     default:
         console.log("You entered a number other than one or two");
 }
